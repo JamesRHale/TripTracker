f44f9464ea407caf589f2f5c0b666cfe323cdeaf
Sean Graves <seankgraves@gmail.com>
2017-01-13 15:08:23 -0500

    Seans changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from "@angular/core";
+import {
+    THIS_EXPR
+} from '../platforms/android/src/main/assets/app/tns_modules/@angular/compiler/src/output/output_ast';
+import { Observable, Subject } from 'rxjs/Rx';
+import { Component, ElementRef, NgZone, ViewChild } from '@angular/core';
+
+import * as geolocation from 'nativescript-geolocation';
+
 
 @Component({
     selector: "my-app",
@@ -9,21 +16,81 @@ export class AppComponent {
     public isTracking:boolean = false
     public noTripsLabel = "Record and log your trips"
 
+    public loc$: Subject<geolocation.Location>;
+    public watchId: number;
+
+
+    public longs: string[] = [];
+    public points: geolocation.Location[];
+
+    public long$: Observable<string>;
+    public points$: Observable<geolocation.Location>;
+
+    @ViewChild("mylabel") myLabel: ElementRef;
+
+    constructor(private _ngZone: NgZone) {
+        this.loc$ = new Subject();
+        this.long$ = this.loc$.asObservable().map(l => `${l.latitude} : ${l.longitude}`);
+        this.long$.subscribe(v => {
+            try {
+                _ngZone.run(() => {
+                    this.longs.push(v);
+                    // this.myLabel.nativeElement.text = this.longs.join('              ');            
+                });
+            } catch(e) {
+                console.log("Error: " + e);
+            }
+        });
+
+        this.points$ = this.loc$.asObservable();
+        this.points$.subscribe(v => {
+            try {
+                _ngZone.run(() => {
+                    this.points.push(v);
+                    // this.myLabel.nativeElement.text = this.longs.join('              ');            
+                });
+            } catch(e) {
+                console.log("Error: " + e);
+            }
+        });
+    }
+
     public startTracking() {
-        this.trackingStarted = true
-        this.isTracking = true
+        this.trackingStarted = true;
+        this.isTracking = true;
+        this.longs = [];
+        this.points = [];
+
+        this.watchId = geolocation.watchLocation((loc) => {
+            if (loc && this.isTracking) {
+                this.loc$.next(loc);
+                // this.myLabel.nativeElement.text = loc.longitude;
+                // console.log("Received location: " + loc.longitude);
+
+                // alert(`Long is ${loc.longitude}`);
+            }
+        }, (e) => {
+            console.log("Error: " + e.message);
+        }, 
+        {
+            desiredAccuracy: 3, 
+            updateDistance: 5, 
+            minimumUpdateTime : 1000
+        }); // Should update every 20 seconds according to Googe documentation. Not verified.
+
     }
 
      public stopTracking() {
-        this.isTracking = false
+        this.isTracking = false;
     }
 
       public resumeTracking() {
-        this.isTracking = true
+        this.isTracking = true;
     }
 
        public completeTracking() {
-        this.isTracking = false
-        this.trackingStarted = false
+        this.isTracking = false;
+        this.trackingStarted = false;
+        geolocation.clearWatch(this.watchId);
     }
 }
--- app/map.component.ts
+++ app/map.component.ts
@@ -1,6 +1,8 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import * as MapView from "nativescript-google-maps-sdk";
 import {registerElement} from "nativescript-angular/element-registry";
+
+import * as geolocation from 'nativescript-geolocation';
 
 // Important - must register MapView plugin in order to use in Angular templates
 registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView);
@@ -9,12 +11,12 @@ registerElement("MapView", () => require("nativescript-google-maps-sdk").MapView
     selector: 'map',
     template: `
     <GridLayout>
-        <MapView (mapReady)="onMapReady($event)">
+        <MapView (mapReady)="onMapReady($event)"
             latitude="{{ latitude }}" 
             longitude="{{ longitude }}" 
             zoom="{{ zoom }}" 
             padding="{{ padding }}" 
-            mapReady="onMapReady"  
+            mapReady="onMapReady"  >
         </MapView>
     </GridLayout>
     `
@@ -24,17 +26,35 @@ export class MapComponent {
     public latitude : number = 45.43;
     public longitude : number = -75.7;
     zoom = 13;
+
+    map;
+
     @ViewChild("MapView") mapView: ElementRef;
+
+    ngOnChanges() {
+        console.log(this.points.length);
+        if(this.map && this.points.length) {
+            let {latitude, longitude} = this.points[this.points.length - 1];
+            if(latitude && longitude){
+                var marker = new MapView.Marker();
+                marker.position = MapView.Position.positionFromLatLng(latitude, longitude);
+                this.map.addMarker(marker);
+            }
+        }
+    }
+
+
+    @Input() points: geolocation.Location[] = [];
 
     //Map events
     onMapReady(args) {
-        var map = args.object;
+        this.map = args.object;
         var marker = new MapView.Marker();
-        marker.position = MapView.Position.positionFromLatLng(-45.0, -75.7);
+        marker.position = MapView.Position.positionFromLatLng(this.latitude, this.longitude);
         marker.title = "Ottawa";
         marker.snippet = "Canada";
 
         marker.userData = { index : 1};
-        map.addMarker(marker);
+        this.map.addMarker(marker);
     }
 }
